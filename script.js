b62a56f3351a0e2c0afc352965c8c76176dba882
jenDoeMoldovan <michalevic@gmail.com>
2021-10-12 17:24:30 +0300

    ......

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-//setting the invader object
-const invader1 = {
-  class: "invader-1",
-  strength: 1,
-};
+const grid = document.querySelector(".grid");
 
-//setting the board on which the invaders will be laid out
-const invadersBoard = document.querySelector(".invadersBoard");
-
-//the function which will build the board and insert the invaders into it
-function startGame(board, invader) {
-  board.classList.add("invadersGrid1");
-
-  for (i = 0; i < 35; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add(invader.class);
-    newDiv.style.backgroundColor = "white";
-    board.appendChild(newDiv);
-  }
-  movingInvaders(board);
+for (let i = 0; i < 225; i++) {
+  const square = document.createElement("div");
+  grid.appendChild(square);
 }
-
-//Setting the function which will move the invaders
-function movingInvaders(invadersOnTheBoard) {
-  const moveX = 30;
-  const moveY = 30;
-}
-startGame(invadersBoard, invader1);
